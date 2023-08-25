d7b277df29b9bc5583b4b77c1f6aa993d8683993
SURAJ SINGH <84866441+surajsingh78271@users.noreply.github.com>
2023-08-25 13:20:38 +0530

    Update connectionDatabase.js

--- db/connectionDatabase.js
+++ db/connectionDatabase.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 
 const connectionDatabase = async (DataBase_URL)=>{
     return await mongoose.connect(DataBase_URL,{
-        dbName:"formDB"   
+        dbName:"formDB",
+        useNewUrlParser: true,
+            useUnifiedTopology: true,
     }).then(()=>{
         console.log("database connected")
     }).catch((error)=>{
